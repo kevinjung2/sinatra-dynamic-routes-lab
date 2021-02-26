7b7640ae4146c8e2f6c9e7ce86a7ab6158a97139
Kevin Jung <kev.jung.ii@gmail.com>
2021-02-25 22:16:37 -0500

    commit tree babcb2a6b3018726bc7042cba3275577ee278340

--- app.rb
+++ app.rb
@@ -3,4 +3,23 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get '/reversename/:name' do
+    erb :reverse
+  end
+
+  get '/square/:number' do
+    erb :square
+  end
+
+  get '/say/:number/:phrase' do
+    erb :repeat
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    erb :merge
+  end
+
+  get '/:operation/:number1/:number2' do
+    erb :math
+  end
 end
